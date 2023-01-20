244d00057b49ce2d4dffe170ca29ebcd20d115e7
Azizah Agha <117432933+AzizahAgha@users.noreply.github.com>
2023-01-20 18:25:22 +0300

    Add files via upload

---
+++ src/components/charts.js
@@ -0,0 +1,445 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import Chart from "react-apexcharts";
+
+export const options = {
+    series: [{
+    name: 'north',
+    data: [{
+        x: 1996,
+        y: 322
+      },
+      {
+        x: 1997,
+        y: 324
+      },
+      {
+        x: 1998,
+        y: 329
+      },
+      {
+        x: 1999,
+        y: 342
+      },
+      {
+        x: 2000,
+        y: 348
+      },
+      {
+        x: 2001,
+        y: 334
+      },
+      {
+        x: 2002,
+        y: 325
+      },
+      {
+        x: 2003,
+        y: 316
+      },
+      {
+        x: 2004,
+        y: 318
+      },
+      {
+        x: 2005,
+        y: 330
+      },
+      {
+        x: 2006,
+        y: 355
+      },
+      {
+        x: 2007,
+        y: 366
+      },
+      {
+        x: 2008,
+        y: 337
+      },
+      {
+        x: 2009,
+        y: 352
+      },
+      {
+        x: 2010,
+        y: 377
+      },
+      {
+        x: 2011,
+        y: 383
+      },
+      {
+        x: 2012,
+        y: 344
+      },
+      {
+        x: 2013,
+        y: 366
+      },
+      {
+        x: 2014,
+        y: 389
+      },
+      {
+        x: 2015,
+        y: 334
+      }
+    ]
+  }, {
+    name: 'south',
+    data: [
+      {
+        x: 1996,
+        y: 162
+      },
+      {
+        x: 1997,
+        y: 90
+      },
+      {
+        x: 1998,
+        y: 50
+      },
+      {
+        x: 1999,
+        y: 77
+      },
+      {
+        x: 2000,
+        y: 35
+      },
+      {
+        x: 2001,
+        y: -45
+      },
+      {
+        x: 2002,
+        y: -88
+      },
+      {
+        x: 2003,
+        y: -120
+      },
+      {
+        x: 2004,
+        y: -156
+      },
+      {
+        x: 2005,
+        y: -123
+      },
+      {
+        x: 2006,
+        y: -88
+      },
+      {
+        x: 2007,
+        y: -66
+      },
+      {
+        x: 2008,
+        y: -45
+      },
+      {
+        x: 2009,
+        y: -29
+      },
+      {
+        x: 2010,
+        y: -45
+      },
+      {
+        x: 2011,
+        y: -88
+      },
+      {
+        x: 2012,
+        y: -132
+      },
+      {
+        x: 2013,
+        y: -146
+      },
+      {
+        x: 2014,
+        y: -169
+      },
+      {
+        x: 2015,
+        y: -184
+      }
+    ]
+  }],
+    chart: {
+    type: 'area',
+    height: 350
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    curve: 'straight'
+  },
+  
+  title: {
+    // text: 'Area with Negative Values',
+    align: 'left',
+    style: {
+      fontSize: '14px'
+    }
+  },
+  xaxis: {
+    type: 'datetime',
+    axisBorder: {
+      show: false
+    },
+    axisTicks: {
+      show: false
+    }
+  },
+  yaxis: {
+    tickAmount: 4,
+    floating: false,
+  
+    labels: {
+      style: {
+        colors: '#8e8da4',
+      },
+      offsetY: -7,
+      offsetX: 0,
+    },
+    axisBorder: {
+      show: false,
+    },
+    axisTicks: {
+      show: false
+    }
+  },
+  fill: {
+    opacity: 0.5
+  },
+  tooltip: {
+    x: {
+      format: "yyyy",
+    },
+    fixed: {
+      enabled: false,
+      position: 'topRight'
+    }
+  },
+  grid: {
+    yaxis: {
+      lines: {
+        offsetX: -30
+      }
+    },
+    padding: {
+      left: 20
+    }
+  }, colors: ["#ff0000", "#43a047"]
+  };
+
+
+  export const baroptions={
+    series: [{
+        name: 'Net Profit',
+        data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+      }],
+      
+        chart: {
+          type: 'bar',
+          height: 350
+        },
+        plotOptions: {
+          bar: {
+            horizontal: false,
+            columnWidth: '55%',
+            endingShape: 'rounded'
+          },
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          show: true,
+          width: 2,
+          colors: ['transparent']
+        },
+        xaxis: {
+          categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+        },
+        yaxis: {
+          title: {
+            text: '$ (thousands)'
+          }
+        },
+        fill: {
+            type: "gradient",
+            gradient: {
+              shadeIntensity: 1,
+              type: "vertical",
+              inverseColors: true,
+              opacityFrom: 0.7,
+              opacityTo: 0.9,
+              colorStops: [
+                {
+                  offset: 0,
+                  color: "#e65100",
+                  opacity:0.7
+                },
+                {
+                  offset: 100,
+                  color: "#0d47a1",
+                  
+                  opacity: 1,
+                },],
+            //   stops: [30, 90, 100]
+            }
+          },
+        tooltip: {
+          y: {
+            formatter: function (val) {
+              return "$ " + val + " thousands"
+            }
+          }
+        }
+      ,
+      colors: ["#0d47a1"]
+    
+    };
+  
+
+
+export default function ApexChart(){
+
+    //------------------line chart----------------------
+     
+    // const [options,setOptions]=useState({
+    //       chart: {
+    //         id: "basic-bar"
+    //       }, colors: ["#f44336", "#247BA0"],
+    //       xaxis: {
+    //         categories: []
+    //       },
+    //             yaxis: [
+    //               {
+    //                 axisTicks: {
+    //                   show: true,
+    //                 },
+    //                 axisBorder: {
+    //                   show: true,
+    //                   color: '#ff0000'
+    //                 },
+    //                 labels: {
+    //                   style: {
+    //                     colors: '#ff0000',
+    //                   }
+    //                 },
+    //                 title: {
+    //                   text: "Income (lakhs)",
+    //                   style: {
+    //                     color: '#ff0000',
+    //                   }
+    //                 },
+    //                 tooltip: {
+    //                   enabled: true
+    //                 }
+    //               },
+    //               {
+    //                 seriesName: 'Income',
+    //                 opposite: true,
+    //                 axisTicks: {
+    //                   show: true,
+    //                 },
+    //                 axisBorder: {
+    //                   show: true,
+    //                   color: '#00E396'
+    //                 },
+    //                 labels: {
+    //                   style: {
+    //                     colors: '#00E396',
+    //                   }
+    //                 },
+    //                 title: {
+    //                   text: "Previous Income (lakhs)",
+    //                   style: {
+    //                     color: '#00E396',
+    //                   }
+    //                 },
+    //               },
+    //               {
+    //                 seriesName: 'Age',
+    //                 opposite: true,
+    //                 axisTicks: {
+    //                   show: true,
+    //                 },
+    //                 axisBorder: {
+    //                   show: true,
+    //                   color: '#FEB019'
+    //                 },
+    //                 labels: {
+    //                   style: {
+    //                     colors: '#FEB019',
+    //                   },
+    //                 },
+    //                 stroke: {
+    //                  curve: 'straight'
+    //                 },
+    //                 title: {
+    //                   text: "Age",
+    //                   style: {
+    //                     color: '#FEB019',
+    //                   }
+    //                 }
+    //               },
+    //             ],
+    //       stroke: {
+    //         curve: 'straight'
+    //       },
+    //       title: {
+    //         text: 'Fundamental Analysis of Employee',
+    //         align: 'left'
+    //       },
+    //       subtitle: {
+    //         text: 'Age/Salary Movements',
+    //         align: 'left'
+    //       }, colors: ["#ff0000", "#43a047","#247BA0"]
+    //     })
+  
+    //     const [series,setSeries]=useState([
+    //       {
+    //         name: "prev_salary",
+    //         type:'bar',
+    //         data: [],
+    //         color:"#ff0000"
+    //       },
+    //       {
+    //         name: "emp_salary",
+    //         type:'bar',
+    //         data: [],
+    //         color: "#247BA0"
+    //       }
+    //       // ,
+    //       // {
+    //       //   name: "emp_age",
+    //       //   type:'bar',
+    //       //   data: [],
+    //       //   color: "#247BA0"
+    //       // },
+    //   ])
+
+  
+  
+  //------------------------pie chart------------------------------
+  
+       
+      
+      
+        
+        
+  
+  //------------------------------getting values---------------------------------
+  
+      
+        }          
---
+++ src/components/dashboard.js
@@ -0,0 +1,492 @@
+import React,{useState} from "react";
+import { Link } from "react-router-dom";
+import {Grid} from '@mui/material'
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Chart from "react-apexcharts";
+import Paper from '@mui/material/Paper';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import TextField from '@mui/material/TextField';
+import {grey} from '@mui/material/colors';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import AddIcon from '@mui/icons-material/Add';
+import Menu from '@mui/material/Menu';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import img1 from './images/fav.png'
+import img2 from './images/whale.png'
+import img3 from './images/bird.png'
+import img4 from './images/snail.png'
+import img5 from './images/jaq.jfif'
+import img6 from './images/matt.jfif'
+import img7 from './images/alex.jfif'
+import {SideBar} from "./sidebar";
+import ApexChart from "./apexchart";
+import { options } from "./charts";
+import { baroptions } from "./charts";
+
+
+
+export default function Dashboard(){
+    const [selected,setSelected]=useState(0);
+  const [show,setShow]=useState(false);
+  const [val, setVal] = React.useState(dayjs('2023-01-17'));
+
+  //-----------------------------icon menu------------------------------
+const [anchorEl, setAnchorEl] = React.useState(null);
+const open = Boolean(anchorEl);
+const handleClick = (event) => {
+  setAnchorEl(event.currentTarget);
+};
+const handleClose = () => {
+  setAnchorEl(null);
+};
+//-----------------------------paper-----------------------------------------      
+
+const Item = styled(Paper)(({ theme }) => ({
+
+    ...theme.typography.body2,
+    
+    textAlign: 'center',
+    padding: theme.spacing(0),
+    
+boxShadow: "0px 0px 9px 4px rgb(0 0 0 / 4%), 0px 5px 0px 10px rgb(0 0 0 / 0%), 0px 8px 30px -26px rgb(0 0 0 / 16%)",
+    borderRadius:"0%",
+    border:"2px solid rgb(218,218,218)",
+    color: "#ff0000, #247BA0,#ffff00"
+    // color: theme.palette.text.secondary,
+  }));
+  
+
+
+  return(
+    
+    <div className="dashboard">
+
+    <div className={show?'hide':'sidebar'}>
+     <div className="top">
+     <IconButton
+       size="large"
+       edge="start"
+       color="inherit"
+       aria-label="menu"
+       sx={{ mr: 2 }}
+     >
+       <MenuIcon onClick={()=>setShow(true)}/>
+     </IconButton>
+       <span className="logoo">givepanel</span>
+     </div>
+     <br></br>
+     
+    
+{/* --------------------------------------------------------------------------------   */}
+
+
+     <div className="menuu">
+       {SideBar.map((item,index)=>{
+         return(
+           <div className={selected===index?'menuItem active':'menuItem'}
+           key={index}
+           onClick={()=>setSelected(index)}
+           >
+            
+             <item.icon className={selected===index?'iconactive':''}/>
+             <span>
+               {item.heading}
+             </span>
+           </div>
+         )
+       })}
+
+       <div>
+
+       </div>   
+
+     </div>
+
+{/* --------------------------------------------------------------------------------   */}    
+     <div className="bottom">
+       {/* <div className="co1 coloroption"></div>
+       <div className="co2 coloroption"></div>
+        */}
+        <div className="menusetting" >
+        <SettingsOutlinedIcon  />
+           <Link className="span">Settings</Link>
+        </div>
+       
+     </div>
+   </div>
+
+
+      <div className="main">
+
+            <div className="headdiv">
+            <div className="div">
+           {show?<IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon onClick={()=>setShow(false)}/>
+          </IconButton>:null}
+            <h5 className="left">Dashboard</h5>
+            </div>
+            <div className="div">
+            <Link class="whatsnew" to='/list' >What's New</Link>  
+            <Avatar sx={{ bgcolor: grey[900] }} className="black" variant="square"> TN</Avatar>
+            <span className="down">Tiger Nixon</span>
+            <React.Fragment>
+            <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open ? 'account-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+          >
+            < ArrowDropDownIcon />
+          </IconButton>
+          <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        <MenuItem>
+          <Avatar /> Profile
+        </MenuItem>
+        <MenuItem>
+          <Avatar /> My Team
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+            </React.Fragment>
+            </div>
+           
+            </div>
+            
+            <hr className="hrr"></hr>
+            
+            <div className="headdiv">
+            <LocalizationProvider  dateAdapter={AdapterDayjs} className="time" >
+              <DatePicker
+             
+                  views={['month','year']}
+                  minDate={dayjs('2022-03-01')}
+                  maxDate={dayjs('2023-01-01')}
+                  value={val}
+                  onChange={(newValue) => {
+                  setVal(newValue);
+                  }}
+                renderInput={(params) => <TextField {...params} />}
+                dayOfWeekFormatter={(day) => `${day}.`}
+                 toolbarFormat="ddd DD MMMM"
+                InputProps={{ sx: { borderRadius: '67px',fontSize:'12px',fontWeight:'700',backgroundColor:'white' } }}
+              />
+              
+            </LocalizationProvider>
+
+            
+               
+               <Link class="buttonn" to='/list' ><AddIcon/>Upload Data</Link>  
+            </div>
+
+
+            <div className="middle-container">
+            <Box sx={{  width: "100%",justifyContent:'center',boxShadow:0}} >
+            <Container maxWidth="md lg">
+            <Grid container rowSpacing={4} columnSpacing={{ xs: 1, sm: 3, md: 3,lg:2 }} alignItems="center">
+            
+            <Grid item xs={12} sm={12} md={4} lg={4}>
+               <Item >
+               <div className="headdivv">
+               <img class="image2" src={img1} alt="emp" width="" />
+               {/* <em>Employess</em> */}
+               <div className="menuuu">
+                  <Link className="topic">Employees</Link>
+                  <h4 className="values">$186</h4>
+                  </div>
+                  </div>
+               </Item>
+               </Grid>
+
+               <Grid item xs={12} sm={12} md={4} lg={4}>
+               <Item >
+               <div className="headdivv">
+               <img class="image2" src={img4} alt="emp" width="" />
+               {/* <em>Employess</em> */}
+               <div className="menuuu">
+                  <Link className="topic">Temporary</Link>
+                  <h4 className="values">$77</h4>
+                  </div>
+                  </div>
+               </Item>
+               </Grid>
+
+               <Grid item xs={12} sm={12} md={4} lg={4}>
+               <Item>
+               <div className="headdivv">
+               <img class="image2" src={img3} alt="emp" width="" />
+               
+               {/* <em>Employess</em> */}
+               <div className="menuuu ">
+                  <Link className="topic">Intern Emp</Link>
+                  <h4 className="values">$678</h4>
+                  </div>
+                  </div>
+               </Item>
+               </Grid>
+
+               
+               <Grid item xs={12} md={6} lg={6}>
+               <div className="headdiv">
+               <h4 className="head">Revenue</h4>
+               <Link className="head2">view</Link>
+               </div>
+               <Item>
+               <Chart
+                options={options}
+                series={options.series}
+                type="line"
+                width="100%"
+                height="250"
+                justifyContent="center"
+               />
+               </Item>
+               </Grid>
+               
+               
+
+               <Grid item  xs={12} md={6} lg={6}>
+               <div className="headdiv">
+               <h4 className="head">Donations</h4>
+               <Link className="head2">view</Link>
+               </div>
+               
+               <Item>
+               <Chart
+                // options={pieOption.options}
+                // series={pieOption.series}
+                options={baroptions}
+                series={baroptions.series}
+                type="bar"
+                width="100%"
+                height="175%"
+               />
+               </Item>   
+               </Grid>
+            </Grid>
+            </Container>
+            </Box>   
+
+            <div className="mid">
+
+               <div className="headdiv">
+               <h4 className="head">Top </h4>
+               <Link className="head2">view</Link>
+               </div>
+             </div>
+
+            <Box sx={{  width: "100%",justifyContent:'center',boxShadow:0}} >
+            <Container maxWidth="md lg">
+            <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 3, md: 3,lg:2 }} alignItems="center">
+               
+            <Grid item xs={12} sm={12} md={4} lg={4}>
+               <Item >
+               <div className="cardd">
+               <div className="headdiv">
+               <div className="values">$34553.98</div>
+               <div className="inf"><InfoOutlinedIcon className="iconss"/></div>
+               </div>
+               </div>
+               <hr></hr>
+               <div className="headdivv">
+               <img class="image1" src={img5} alt="emp" width="" />
+               {/* <em>Employess</em> */}
+               <div className="menuuu">
+                  <Link className="topicc">Ron Hill</Link>
+                  <h4 className="valuess">25 Dec 20</h4>
+                  </div>
+                  </div>
+               </Item>
+               </Grid>
+
+               <Grid item xs={12} sm={12} md={4} lg={4}>
+               <Item >
+               <div className="cardd">
+               <div className="headdiv">
+               <div className="values">$3450.90</div>
+               <div className="inf"><InfoOutlinedIcon className="iconss"/></div>
+               </div>
+               </div>
+               <hr></hr>
+               <div className="headdivv">
+               <img class="image1" src={img6} alt="emp" width="" />
+               {/* <em>Employess</em> */}
+               <div className="menuuu">
+                  <Link className="topicc">Matt Watson</Link>
+                  <h4 className="valuess">22 dec 21</h4>
+                  </div>
+                  </div>
+               </Item>
+               </Grid>
+
+               <Grid item xs={12} sm={12} md={4} lg={4}>
+               <Item >
+               <div className="cardd">
+               <div className="headdiv">
+               <div className="values">$450.90</div>
+               <div className="inf"><InfoOutlinedIcon className="iconss"/></div>
+               </div>
+               </div>
+               <hr></hr>
+               <div className="headdivv">
+               <img class="image1" src={img7} alt="emp" width="" />
+               {/* <em>Employess</em> */}
+               <div className="menuuu">
+                  <Link className="topicc">Alex Jacob</Link>
+                  <h4 className="valuess">25 Dec 21</h4>
+                  </div>
+                  </div>
+               </Item>
+               </Grid>
+
+
+
+               <Grid item xs={12} sm={12} md={4} lg={4}>
+               <Item >
+               <div className="cardd">
+               <div className="headdiv">
+               <div className="values">$467</div>
+               <div className="inf"><InfoOutlinedIcon/></div>
+               </div>
+               </div>
+               <hr></hr>
+               <div className="headdivv">
+               <img class="image2" src={img1} alt="emp" width="" />
+               {/* <em>Employess</em> */}
+               <div className="menuuu">
+                  <Link className="topic">Employees</Link>
+                  <h4 className="values">$186</h4>
+                  </div>
+                  </div>
+               </Item>
+               </Grid>
+
+               
+               <Grid item xs={12} sm={12} md={4} lg={4}>
+               <div className="headdiv">
+               <h4 className="head"></h4>
+               <Link className="head2"></Link>
+               </div>
+               <Item >
+               <div className="cardd">
+               <div className="headdiv">
+               <div className="values">$27</div>
+               <div className="inf"><InfoOutlinedIcon/></div>
+               </div>
+               </div>
+               <hr></hr>
+               <div className="headdivv">
+               <img class="image2" src={img1} alt="emp" width="" />
+               {/* <em>Employess</em> */}
+               <div className="menuuu">
+                  <Link className="topic">Employees</Link>
+                  <h4 className="valuess">$186</h4>
+                  </div>
+                  </div>
+               </Item>
+               </Grid>
+
+               <Grid item xs={12} sm={12} md={4} lg={4}>
+               <div className="headdiv">
+               <h4 className="head"></h4>
+               <Link className="head2"></Link>
+               </div>
+               <Item >
+               <div className="cardd">
+               <div className="headdiv">
+               <div className="values">$954</div>
+               <div className="inf"><InfoOutlinedIcon/></div>
+               </div>
+               </div>
+               <hr></hr>
+               <div className="headdivv">
+               <img class="image2" src={img1} alt="emp" width="" />
+               {/* <em>Employess</em> */}
+               <div className="menuuu">
+                  <Link className="topic">Employees</Link>
+                  <h4 className="values">$186</h4>
+                  </div>
+                  </div>
+               </Item>
+               </Grid>
+ 
+
+            </Grid>
+            </Container>
+            </Box>
+
+          
+            
+        </div>
+        </div>
+
+</div>
+  )
+}
